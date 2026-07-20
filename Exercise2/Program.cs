8518b7787af547d7b505f33d70b42be0dd690e77
ahmed-razic <ahmed.razic@gmail.com>
2026-07-20 23:27:14 +0200

    commit

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -5,22 +5,28 @@
         static void Main(string[] args)
         {
             Score score1 = new Score();
+            Console.WriteLine($"Name: {score1.name}, Points: {score1.points}, Level: {score1.level}");
 
+            Score score2 = new Score("Player1", 5000, 4);
+            Console.WriteLine($"Name: {score2.name}, Points: {score2.points}, Level: {score2.level}");
         }
 
-        class Score
+        class Score    
         {
             public string name;
             public int points;
             public int level;
 
-            public Score()
+            public Score() : this("unknown", 0, 1)
             {
-                name = "Ahmed";
-                points = 0;
-                level = 1;
             }
 
+            public Score(string name, int points, int level)
+            {
+                this.name = name;
+                this.points = points;
+                this.level = level;
+            }
             public bool EarnedStar() => (points / level) > 1000;
         }
     }
