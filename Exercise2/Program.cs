372f55f599b318afbbd686b5c106d48e19809388
ahmed-razic <ahmed.razic@gmail.com>
2026-07-20 23:11:28 +0200

    commit

---
+++ Exercise2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Score score1 = new Score();
+
+        }
+
+        class Score
+        {
+            public string name;
+            public int points;
+            public int level;
+
+            public Score()
+            {
+                name = "Ahmed";
+                points = 0;
+                level = 1;
+            }
+
+            public bool EarnedStar() => (points / level) > 1000;
+        }
+    }
+}
